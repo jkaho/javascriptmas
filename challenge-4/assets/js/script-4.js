cf2c0ee35072324258e460920f844ff6fced3589
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:02:10 +1100

    4: Add solution

--- challenge-4/assets/js/script-4.js
+++ challenge-4/assets/js/script-4.js
@@ -2,3 +2,12 @@
 // Given a year, return the century it is in
 // The first century spans from the year 1 up to and including the year 11
 // The second spans from year 101 up to and including the year 200, etc. 
+
+// -------My Solution------- // 
+function centuryFromYear(num) {
+    if (num % 100 !== 0) {
+        console.log(Math.floor(num / 100 + 1));
+    } else {
+        console.log(Math.floor(num / 100));
+    }
+}
