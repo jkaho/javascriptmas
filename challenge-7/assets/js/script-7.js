20230460f92519422858508b8cf26ef2d654ae78
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 19:33:34 +1100

    7: Modify js to add UI functionality

--- challenge-7/assets/js/script-7.js
+++ challenge-7/assets/js/script-7.js
@@ -4,7 +4,28 @@
 // Consonants are given a value of 2
 // Return the sum of the letters in the input string 
 
-function countVowelConsonant(str) {
+// -------My Solution------- // 
+// function countVowelConsonant(str) {
+//     var sum = 0;
+
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+//             sum += 1;
+//         } else {
+//             sum += 2;
+//         }
+//     }
+
+//     return sum;
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var stringInputEl = document.getElementById("string-input");
+var sumEl = document.getElementById("sum");
+
+function countVowelConsonant() {
+    var str = stringInputEl.value.toLowerCase();
     var sum = 0;
 
     for (var i = 0; i < str.length; i++) {
@@ -14,6 +35,8 @@ function countVowelConsonant(str) {
             sum += 2;
         }
     }
-    
-    return sum;
+
+    sumEl.textContent = sum;
 }
+
+submitBtn.addEventListener("click", countVowelConsonant);
