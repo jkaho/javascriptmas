640f36115417ffe6815725fae67241d3c020998f
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 23:13:17 +1100

    8: Modify js to add challenge task comments

---
+++ challenge-8/assets/js/script-8.js
@@ -0,0 +1,8 @@
+/*
+// -------Challenge------- // 
+In this challenge a casino has asked you to make an online dice that works just like 
+it would in real life. Using the pre-made dice face that represents ‘one’, make the 
+faces for ‘two’, ‘three’, ‘four’, ‘five’ and ‘six’. Now when the users clicks the 
+dice on the screen the dice is expected to show one of the faces randomly.
+*/
+
