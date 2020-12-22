c234a2351ddc910e3796b148c57ab08c840e3b68
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:55:33 +1100

    6: Add solution

--- challenge-6/assets/js/script-6.js
+++ challenge-6/assets/js/script-6.js
@@ -1,3 +1,11 @@
 // -------Challenge------- // 
 // Given an array of strings, sort them in the order of increasing lengths
 // If two strings have the same length, their relative order must be the same as in the initial array
+
+// -------My Solution------- // 
+function sortByLength(strs) {
+    strs.sort(function(a, b) {
+        return a.length - b.length;
+    })
+    return strs;
+}
