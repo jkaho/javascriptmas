e1226895bceeb56774789b258522c2132e2f382e
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 23:39:43 +1100

    8: Modify js to add function for rolling dice

--- challenge-8/assets/js/script-8.js
+++ challenge-8/assets/js/script-8.js
@@ -7,8 +7,60 @@ dice on the screen the dice is expected to show one of the faces randomly.
 */
 
 // -------My Solution------- // 
-var dice = document.getElementsByClassName("dice");
+var dotDivs = document.getElementsByClassName("dot");
 
 function rollingDice() {
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    if (diceNumber === 1) {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("one")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    } else if (diceNumber === 2) {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("two")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    } else if (diceNumber === 3) {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("three")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    } else if (diceNumber === 4) {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("four")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    } else if (diceNumber === 5) {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("five")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    } else {
+        for (var i = 0; i < dotDivs.length; i++) {
+            if (dotDivs[i].className.includes("six")) {
+                dotDivs[i].classList.remove("hide");
+            } else {
+                dotDivs[i].classList.add("hide");
+            }
+        }
+    }
 }
+
+rollingDice();
