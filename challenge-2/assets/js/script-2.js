3ee61762c1ad99a8e0ca9c338b940c26bdc7e041
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 14:05:38 +1100

    2: Modify js to add challenge task comments

---
+++ challenge-2/assets/js/script-2.js
@@ -0,0 +1,5 @@
+// -------Challenge------- // 
+// You have deposited a specific amount of dollars into your bank account
+// Each year, your balance increases at the same growth rate
+// Find out how long it would take for your balance to pass a specific threshold
+// Assumption: you don't make any additional deposits 
