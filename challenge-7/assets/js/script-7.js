6521c642d00db844b11b2e058efffc6620cdd4b6
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 19:19:00 +1100

    7: Modify js to add challenge task comments

---
+++ challenge-7/assets/js/script-7.js
@@ -0,0 +1,5 @@
+// -------Challenge------- // 
+// You are given a string s that consist of only lowercase English letters
+// Vowels are given a value of 1
+// Consonants are given a value of 2
+// Return the sum of the letters in the input string 
