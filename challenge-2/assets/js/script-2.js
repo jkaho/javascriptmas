33a28792f58be7a0dea2caf269f8fb50124b2388
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 16:12:52 +1100

    2: Add solution

--- challenge-2/assets/js/script-2.js
+++ challenge-2/assets/js/script-2.js
@@ -3,3 +3,17 @@
 // Each year, your balance increases at the same growth rate
 // Find out how long it would take for your balance to pass a specific threshold
 // Assumption: you don't make any additional deposits 
+
+// -------My Answer------- // 
+function depositProfit(deposit, rate, threshold) {
+    var year = 1;
+    for (var i = 0; i <= 1; i++) {
+        var balance = deposit * ((100 + rate) / 100) ** year;
+        if (balance < threshold) {
+            i--;
+            year++;
+        } else {
+            return year;
+        }
+    }    
+}
