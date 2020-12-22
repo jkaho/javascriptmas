00a5ee8f443cc693ac260c62e1bed898c36a76e6
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:43:15 +1100

    5: Modify js to add UI functionality

--- challenge-5/assets/js/script-5.js
+++ challenge-5/assets/js/script-5.js
@@ -4,11 +4,31 @@
 // Your result must be a string
 
 // -------My Solution------- // 
-function reverseAString(str) {
+// function reverseAString(str) {
+//     var reversedString = "";
+
+//     for (var i = 1; i <= str.length; i++) {
+//         reversedString += str[str.length - i];
+//     }
+
+//     return reversedString;
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var stringInputEl = document.getElementById("string-input");
+var reversedStringEl = document.getElementById("reversed-string");
+
+function reverseAString() {
+    var str = stringInputEl.value;
     var reversedString = "";
+
     for (var i = 1; i <= str.length; i++) {
         reversedString += str[str.length - i];
     }
-    return reversedString;
+
+    reversedStringEl.textContent = reversedString;
 }
+
+submitBtn.addEventListener("click", reverseAString);
 
