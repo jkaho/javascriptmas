74b5c9fefb9830fc2e437fab5c19e2c37b2fb0e9
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 13:59:38 +1100

    1: Modify html and js to add UI functionality

--- challenge-1/assets/js/script-1.js
+++ challenge-1/assets/js/script-1.js
@@ -5,11 +5,31 @@
 // Individual pieces of candy cannot be split
 // Determine how many pieces of candy will be eaten by all the children together
 
-// -------Answer------- // 
-function candies(children, candy) {
-    var portionEach = Math.floor(candy / children);
-    var candies = portionEach * children;
-    return candies;
-}
+// -------My Answer------- // 
+// function candies(children, candy) {
+//     var portionEach = Math.floor(candy / children);
+//     var candies = portionEach * children;
+//     return candies;
+// }
 
 // -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var childrenInputEl = document.getElementById("children-input");
+var candyInputEl = document.getElementById("candy-input");
+var portionEl = document.getElementById("portion");
+var candyEl = document.getElementById("candies");
+var leftoverEl = document.getElementById("leftover");
+
+function renderCandies() {
+    var children = childrenInputEl.value;
+    var candy = candyInputEl.value;
+    var portionEach = Math.floor(candy / children);
+    var candies = portionEach * children;
+    var leftover = candy % children;
+
+    portionEl.textContent = portionEach + " candies";
+    candyEl.textContent = candies;
+    leftoverEl.textContent = leftover;
+}
+
+submitBtn.addEventListener("click", renderCandies);
