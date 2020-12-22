2793242f14ca6582df989410dc14dfe3e8dc3944
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 16:35:13 +1100

    2: Modify js to add UI functionality

--- challenge-2/assets/js/script-2.js
+++ challenge-2/assets/js/script-2.js
@@ -5,15 +5,44 @@
 // Assumption: you don't make any additional deposits 
 
 // -------My Solution------- // 
-function depositProfit(deposit, rate, threshold) {
+// function depositProfit(deposit, rate, threshold) {
+//     var year = 1;
+//     for (var i = 0; i <= 1; i++) {
+//         var balance = deposit * ((100 + rate) / 100) ** year;
+//         if (balance < threshold) {
+//             i--;
+//             year++;
+//         } else {
+//             return year;
+//         }
+//     }    
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var depositInputEl = document.getElementById("deposit-input");
+var rateInputEl = document.getElementById("rate-input");
+var thresholdInputEl = document.getElementById("threshold-input");
+var yearsEl = document.getElementById("years");
+
+function renderDepositProfit() {
+    var deposit = parseInt(depositInputEl.value);
+    var rate = parseInt(rateInputEl.value);
+    var threshold = parseInt(thresholdInputEl.value);
     var year = 1;
+
     for (var i = 0; i <= 1; i++) {
         var balance = deposit * ((100 + rate) / 100) ** year;
+
         if (balance < threshold) {
             i--;
             year++;
         } else {
-            return year;
+            yearsEl.textContent = year;
         }
-    }    
+    } 
+    balance = 0;
 }
+
+submitBtn.addEventListener("click", renderDepositProfit);
+
