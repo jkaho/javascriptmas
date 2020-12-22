096fb6f85d24bb6d2920d534156a4cef988f518a
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:36:41 +1100

    5: Add solution

--- challenge-5/assets/js/script-5.js
+++ challenge-5/assets/js/script-5.js
@@ -2,3 +2,13 @@
 // Reverse the provided string
 // You may need to turn the string into an array before you can reverse it
 // Your result must be a string
+
+// -------My Solution------- // 
+function reverseAString(str) {
+    var reversedString = "";
+    for (var i = 1; i <= str.length; i++) {
+        reversedString += str[str.length - i];
+    }
+    return reversedString;
+}
+
