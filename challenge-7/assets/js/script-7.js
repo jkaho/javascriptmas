5546e2688d429e25d934cfa5c1edb23c0f70764c
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 19:23:10 +1100

    7: Add solution

--- challenge-7/assets/js/script-7.js
+++ challenge-7/assets/js/script-7.js
@@ -3,3 +3,17 @@
 // Vowels are given a value of 1
 // Consonants are given a value of 2
 // Return the sum of the letters in the input string 
+
+function countVowelConsonant(str) {
+    var sum = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+            sum += 1;
+        } else {
+            sum += 2;
+        }
+    }
+    
+    return sum;
+}
