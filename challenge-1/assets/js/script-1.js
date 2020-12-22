79875ad5aa7f440eb4fbb77c1a67a5dbbc2c361c
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 13:40:11 +1100

    1: Modify js to add comments, modify html to restructure UI

--- challenge-1/assets/js/script-1.js
+++ challenge-1/assets/js/script-1.js
@@ -1,11 +1,15 @@
+// -------Challenge------- // 
 // n children have got m pieces of candy 
 // They want to eat as much candy as they can
 // Each child must eat exactly the same amount of candy as any other child
 // Individual pieces of candy cannot be split
 // Determine how many pieces of candy will be eaten by all the children together
 
+// -------Answer------- // 
 function candies(children, candy) {
     var portionEach = Math.floor(candy / children);
     var candies = portionEach * children;
     return candies;
 }
+
+// -------UI------- // 
