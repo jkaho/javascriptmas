cd37788b8999cb684076a0888e2d1b38d9b3f5aa
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:50:26 +1100

    6: Modify js to add challenge task comments

---
+++ challenge-6/assets/js/script-6.js
@@ -0,0 +1,3 @@
+// -------Challenge------- // 
+// Given an array of strings, sort them in the order of increasing lengths
+// If two strings have the same length, their relative order must be the same as in the initial array
