ced2a11f669d820e5ad04e7b5e335751c5f94ecb
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 13:19:37 +1100

    1: Change file names, modify 'script-1.js' to add instructions

---
+++ Challenge-1/assets/js/script-1.js
@@ -0,0 +1,5 @@
+// n children have got m pieces of candy 
+// They want to eat as much candy as they can
+// Each child must eat exactly the same amount of candy as any other child
+// Individual pieces of candy cannot be split
+// Determine how many pieces of candy will be eaten by all the children together
