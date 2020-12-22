5f4cd5473029f60de4840093ec424a9d549c75c8
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 18:59:59 +1100

    6: Modify js & html to add html element variables in js

--- challenge-6/assets/js/script-6.js
+++ challenge-6/assets/js/script-6.js
@@ -3,6 +3,19 @@
 // If two strings have the same length, their relative order must be the same as in the initial array
 
 // -------My Solution------- // 
+// function sortByLength(strs) {
+//     strs.sort(function(a, b) {
+//         return a.length - b.length;
+//     })
+//     return strs;
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var stringInputEl = document.getElementById("string-input");
+var stringArr = document.getElementById("string-array");
+var sortedArr = document.getElementById("sorted-array");
+
 function sortByLength(strs) {
     strs.sort(function(a, b) {
         return a.length - b.length;
