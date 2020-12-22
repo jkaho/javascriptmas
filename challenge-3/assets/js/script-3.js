bd22c49558c07f01dd413235c530c3cf95a211f4
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 17:05:42 +1100

    3: Add solution

--- challenge-3/assets/js/script-3.js
+++ challenge-3/assets/js/script-3.js
@@ -2,3 +2,15 @@
 // Write a function that splits an array (1st argument) into groups the length of size (2nd argument)
 // Return them as a two-dimensional array
 
+// -------My Solution------- // 
+function chunkyMonkey(values, size) {
+    var arr = [];
+    var sliceEnd = size;
+    for (var i = 0; i < values.length; i += size) {
+        var slicedArr = values.slice(i, sliceEnd);
+        sliceEnd += size;
+
+        arr.push(slicedArr);
+    }
+    return arr;
+}
