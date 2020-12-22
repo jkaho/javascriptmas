c2d2aba3cf28087709ebe566601e53e15a46a663
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-22 17:53:44 +1100

    4: Modify js to add challenge task comments

---
+++ challenge-4/assets/js/script-4.js
@@ -0,0 +1,4 @@
+// -------Challenge------- // 
+// Given a year, return the century it is in
+// The first century spans from the year 1 up to and including the year 11
+// The second spans from year 101 up to and including the year 200, etc. 
