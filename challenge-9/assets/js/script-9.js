dac786dfb6a52960759936c446378580627aa469
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 17:30:16 +1100

    9: Modify js to add challenge task comments

---
+++ challenge-9/assets/js/script-9.js
@@ -0,0 +1,6 @@
+// -------Challenge------- // 
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num 
+// The first two numbers in the Fibonacci sequence are 1 and 1 
+// Every additional number in the sequence is the sum of the two previous numbers
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8
+
