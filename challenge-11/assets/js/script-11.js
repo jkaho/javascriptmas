ec11a3e3927e7bfab70d1ec67523daee7831fa96
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 23:34:23 +1100

    10: Modify js to sort array argument

--- challenge-11/assets/js/script-11.js
+++ challenge-11/assets/js/script-11.js
@@ -3,3 +3,13 @@
 // Assume that you are jumping from the point with coordinate 0 to the right
 // You are allowed only to make jumps of the same length represented by some integer
 // Find the minimal length of the jump enough to avoid all the obstacles
+
+// -------UI------- // 
+function avoidObstacles(nums) {
+    var sortedArr = nums.sort(function(a, b) {
+        return a - b;
+    });
+    console.log(sortedArr);
+}
+
+avoidObstacles([5, 3, 6, 7, 9]);
