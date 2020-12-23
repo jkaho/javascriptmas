ab29c69168d0240ce3f0acca8e65bc5699258b17
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 17:39:26 +1100

    9: Modify js to add function that creates Fibonacci sequence

--- challenge-9/assets/js/script-9.js
+++ challenge-9/assets/js/script-9.js
@@ -4,3 +4,14 @@
 // Every additional number in the sequence is the sum of the two previous numbers
 // The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8
 
+// -------My Solution------- // 
+function sumOddFibonacciNumbers(num) {
+    var fibonacciSequence = [1, 1];
+    for (var i = 0; i <= num; i++) {
+        var fibonacciNumber = fibonacciSequence[i] + fibonacciSequence[i + 1];
+        fibonacciSequence.push(fibonacciNumber);
+        console.log(fibonacciSequence);
+    }
+}
+
+sumOddFibonacciNumbers(10);
