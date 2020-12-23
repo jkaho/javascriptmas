27746f755fe94d94ef482c7045125d84dea0fb76
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 20:07:54 +1100

    10: Modify js to add click event listener for 'Add to Array' btn

--- challenge-10/assets/js/script-10.js
+++ challenge-10/assets/js/script-10.js
@@ -3,8 +3,43 @@
 // Return that product
 
 // -------My Solution------- // 
-function adjacentElementsProduct(nums) {
-    var adjacentProducts = [];
+// function adjacentElementsProduct(nums) {
+//     var adjacentProducts = [];
+//     for (var i = 0; i < nums.length - 1; i++) {
+//         var product = nums[i] * nums[i + 1];
+//         adjacentProducts.push(product);
+//     }
+
+//     var sortedProducts = adjacentProducts.sort(function(a, b) {
+//         return a - b;
+//     });
+
+//     return sortedProducts[sortedProducts.length - 1];
+// }
+
+// -------UI------- // 
+var numBtn = document.getElementById("submit-num");
+var submitBtn = document.getElementById("submit");
+var numInputEl = document.getElementById("num");
+var arrEl = document.getElementById("integer-arr");
+var largestProductEl = document.getElementById("largest-product");
+
+var adjacentProducts = [];
+
+function addIntegerToArr() {
+    if (numInputEl.value !== "") {
+        var nums = numInputEl.value;
+        adjacentProducts.push(nums);
+    } else {
+        alert("Invalid input. Please type an integer.")
+    }
+
+    arrEl.textContent = adjacentProducts;
+}
+
+numBtn.addEventListener("click", addIntegerToArr);
+
+function adjacentElementsProduct() {
     for (var i = 0; i < nums.length - 1; i++) {
         var product = nums[i] * nums[i + 1];
         adjacentProducts.push(product);
