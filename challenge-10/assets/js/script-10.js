8ea93df4c0a54cabbd2abb539ee6ca9340d1ffc6
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 18:58:21 +1100

    10: Add solution

--- challenge-10/assets/js/script-10.js
+++ challenge-10/assets/js/script-10.js
@@ -1,3 +1,18 @@
 // -------Challenge------- // 
 // Given an array of integers find the pair of adjacent elements that has the largest product
 // Return that product
+
+// -------My Solution------- // 
+function adjacentElementsProduct(nums) {
+    var adjacentProducts = [];
+    for (var i = 0; i < nums.length - 1; i++) {
+        var product = nums[i] * nums[i + 1];
+        adjacentProducts.push(product);
+    }
+
+    var sortedProducts = adjacentProducts.sort(function(a, b) {
+        return a - b;
+    });
+
+    return sortedProducts[sortedProducts.length - 1];
+}
