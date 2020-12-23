5182f49f5e5858a637a168d1a6bc70361bca3af5
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-23 18:24:06 +1100

    9: Modify js to add UI functionality

--- challenge-9/assets/js/script-9.js
+++ challenge-9/assets/js/script-9.js
@@ -5,7 +5,34 @@
 // The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8
 
 // -------My Solution------- // 
-function sumOddFibonacciNumbers(num) {
+// function sumOddFibonacciNumbers(num) {
+//     var fibonacciSequence = [1, 1];
+
+//     for (var i = 0; fibonacciSequence[i] + fibonacciSequence[i + 1] <= num; i++) {
+//         var fibonacciNumber = fibonacciSequence[i] + fibonacciSequence[i + 1];
+//         fibonacciSequence.push(fibonacciNumber);
+//     }
+
+//     var fibonacciSum = 0;
+
+//     for (var i = 0; i < fibonacciSequence.length; i++) {
+//         if (fibonacciSequence[i] % 2 !== 0) {
+//             fibonacciSum += fibonacciSequence[i];
+//         }
+//     }
+
+//     return fibonacciSum;
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var numInputEl = document.getElementById("num");
+var fibonacciSequenceEl = document.getElementById("sequence");
+var oddNumberEl = document.getElementById("odd");
+var sumEl = document.getElementById("sum");
+
+function sumOddFibonacciNumbers() {
+    var num = numInputEl.value;
     var fibonacciSequence = [1, 1];
 
     for (var i = 0; fibonacciSequence[i] + fibonacciSequence[i + 1] <= num; i++) {
@@ -14,12 +41,18 @@ function sumOddFibonacciNumbers(num) {
     }
 
     var fibonacciSum = 0;
+    var oddNumbers = [];
 
     for (var i = 0; i < fibonacciSequence.length; i++) {
         if (fibonacciSequence[i] % 2 !== 0) {
             fibonacciSum += fibonacciSequence[i];
+            oddNumbers.push(fibonacciSequence[i]);
+            oddNumberEl.textContent = oddNumbers;
         }
     }
 
-    return fibonacciSum;
+    fibonacciSequenceEl.textContent = fibonacciSequence;
+    sumEl.textContent = fibonacciSum;
 }
+
+submitBtn.addEventListener("click", sumOddFibonacciNumbers);
