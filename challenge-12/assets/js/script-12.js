418415fab19c1d861d7d6eedf2714217cd7f7966
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-25 17:38:08 +1100

    12: Add solution

--- challenge-12/assets/js/script-12.js
+++ challenge-12/assets/js/script-12.js
@@ -3,14 +3,26 @@
 
 function validTime(str) {
     var newStr = str.split("");
-    if (newStr.length !== 4 || newStr[2] !== ":" || parseInt(newStr[0]) === NaN) {
-        // invalid format
-        alert("Invalid input. The format of your input should be: 00:00");
-    } else if (newStr[0] === 3 || newStr[0] === 4 || newStr[0] === 5 || newStr[0] === 6 || newStr[0] === 7 || newStr[0] === 8 || newStr[0] === 9) {
-        alert("Your input is NOT a correct time representation of the 24-hr clock.")
+    console.log(newStr);
+    if (newStr.length !== 5 || newStr[2] !== ":" || parseInt(newStr[0]) === NaN) {
+        return false;
+    } else if (parseInt(newStr[0]) === 3 || parseInt(newStr[0]) === 4 || parseInt(newStr[0]) === 5 || parseInt(newStr[0]) === 6 || parseInt(newStr[0]) === 7 || parseInt(newStr[0]) === 8 || parseInt(newStr[0]) === 9) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 4) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 5) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 6) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 7) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 8) {
+        return false;
+    } else if (parseInt(newStr[0]) === 2 && parseInt(newStr[1]) === 9) {
+        return false;
+    } else if (parseInt(newStr[3]) === 6 || parseInt(newStr[3]) === 7 || parseInt(newStr[3]) === 8 || parseInt(newStr[3]) === 9) {
+        return false;
     } else {
-        alert("Your input is a corect time representation of the 24-hour clock!")
+        return true;
     }
 }
-
-validTime("hello")
