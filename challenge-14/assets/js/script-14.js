cc8f82e344a6acbc140a56e6b088ed5800864ce3
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-25 18:24:28 +1100

    14: Add solution

--- challenge-14/assets/js/script-14.js
+++ challenge-14/assets/js/script-14.js
@@ -1,3 +1,21 @@
 // -------Challenge------- // 
 // Given an array of integers, find the maximal absolute difference between any two of its adjacent elements
 
+// -------My Solution------- // 
+function arrayMaximalAdjacentDifference(nums) {
+    function findDifference(a, b) {
+        return Math.abs(a - b);
+    }
+
+    var differences = [];
+    for (var i = 0; i < nums.length - 1; i++) {
+        var difference = findDifference(nums[i], nums[i + 1]);
+        differences.push(difference);
+    }
+
+    differences.sort(function(a, b) {
+        return a - b;
+    })
+
+    return differences[differences.length - 1];
+}
