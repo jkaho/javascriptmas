4da4f7c04c905419614e1ca8d133db0eed53ec6a
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-25 17:56:31 +1100

    13: Add solution

--- challenge-13/assets/js/script-13.js
+++ challenge-13/assets/js/script-13.js
@@ -1,2 +1,9 @@
 // -------Challenge------- // 
 // Given an array of integers, remove each kth element from it
+
+// -------My Solution------- // 
+function extractEachKth(nums, index) {
+    var extractedArr = nums.filter(num => num % index !== 0);
+    return extractedArr;
+}
+
