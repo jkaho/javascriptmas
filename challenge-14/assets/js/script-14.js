3a9ec413cd4f9816b4f3afcd46597bcf3eb1f602
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-25 18:32:38 +1100

    14: Modify js to add event listener to 'Add to Array' btn

--- challenge-14/assets/js/script-14.js
+++ challenge-14/assets/js/script-14.js
@@ -2,6 +2,43 @@
 // Given an array of integers, find the maximal absolute difference between any two of its adjacent elements
 
 // -------My Solution------- // 
+// function arrayMaximalAdjacentDifference(nums) {
+//     function findDifference(a, b) {
+//         return Math.abs(a - b);
+//     }
+
+//     var differences = [];
+//     for (var i = 0; i < nums.length - 1; i++) {
+//         var difference = findDifference(nums[i], nums[i + 1]);
+//         differences.push(difference);
+//     }
+
+//     differences.sort(function(a, b) {
+//         return a - b;
+//     })
+
+//     return differences[differences.length - 1];
+// }
+
+// -------UI------- // 
+var addBtn = document.getElementById("add");
+var submitBtn = document.getElementById("submit");
+var integerInputEl = document.getElementById("integer");
+var integerArr = document.getElementById("integer-array");
+var maximalDiffEl = document.getElementById("maximal-diff");
+
+var nums = [];
+
+function addToArray() {
+    var integer = parseInt(integerInputEl.value);
+    nums.push(integer);
+    integerArr.textContent = nums;
+
+    integerInputEl.value = "";
+}
+
+addBtn.addEventListener("click", addToArray);
+
 function arrayMaximalAdjacentDifference(nums) {
     function findDifference(a, b) {
         return Math.abs(a - b);
