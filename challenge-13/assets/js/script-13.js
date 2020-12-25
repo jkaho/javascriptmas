1808e946656d8732e028526e309890b35d3ebd1a
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-25 18:05:06 +1100

    13: Modify js to add event listener to 'Add to Array' btn

--- challenge-13/assets/js/script-13.js
+++ challenge-13/assets/js/script-13.js
@@ -2,8 +2,30 @@
 // Given an array of integers, remove each kth element from it
 
 // -------My Solution------- // 
+// function extractEachKth(nums, index) {
+//     var extractedArr = nums.filter(num => num % index !== 0);
+//     return extractedArr;
+// }
+
+// -------UI------- // 
+var addBtn = document.getElementById("add");
+var submitBtn = document.getElementById("submit");
+var numInputEl = document.getElementById("num");
+var kInputEl = document.getElementById("k");
+var arrEl = document.getElementById("num-array");
+var extractedArrEl = document.getElementById("extracted-array");
+
+nums = [];
+
+function addToArray() {
+    var num = numInputEl.value;
+    nums.push(num);
+    arrEl.textContent = nums;
+}
+
+addBtn.addEventListener("click", addToArray);
+
 function extractEachKth(nums, index) {
     var extractedArr = nums.filter(num => num % index !== 0);
     return extractedArr;
 }
-
