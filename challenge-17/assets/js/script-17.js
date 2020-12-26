3f360f8f9b32232cb9f5ebf000675212a59dce44
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 18:24:12 +1100

    17: Modify js to add UI functionality

--- challenge-17/assets/js/script-17.js
+++ challenge-17/assets/js/script-17.js
@@ -2,13 +2,32 @@
 // Given a string, find the number of different characters in it 
 
 // -------My Solution------- // 
-function differentSymbolsNaive(str) {
+// function differentSymbolsNaive(str) {
+//     var chars = [];
+//     for (var i = 0; i < str.length; i++) {
+//         if (chars.includes(str[i]) === false) {
+//             chars.push(str[i])
+//         }
+//     }
+//     return chars.length;
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var inputEl = document.getElementById("input");
+var characterEl = document.getElementById("chars");
+
+function differentSymbolsNaive() {
+    var str = inputEl.value;
     var chars = [];
+
     for (var i = 0; i < str.length; i++) {
         if (chars.includes(str[i]) === false) {
             chars.push(str[i])
         }
     }
-    return chars.length;
+
+    characterEl.textContent = chars.length;
 }
 
+submitBtn.addEventListener("click", differentSymbolsNaive);
