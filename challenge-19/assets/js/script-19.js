182dcbdf1cea36050c2c149f995c7c03468758a8
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 01:08:32 +1100

    19: Add 'challenge-19' folder w/ basic html & js files

---
+++ challenge-19/assets/js/script-19.js
@@ -0,0 +1,4 @@
+// -------Challenge------- // 
+// Check whether the given string is a subsequence of the plaintext alphabet 
+// If characters in the string are not in alphabetical order, output should be false
+// If there are duplicate characters, output should be false
