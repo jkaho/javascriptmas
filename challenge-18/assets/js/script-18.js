39d0440c696a30863ea2cecbc80c9d16b259145d
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 00:54:30 +1100

    18: Modify js to add html variables

--- challenge-18/assets/js/script-18.js
+++ challenge-18/assets/js/script-18.js
@@ -6,6 +6,38 @@
 // If no such value can be found, store -1 instead
 
 // -------My Solution------- // 
+// function arrayPreviousLess(nums) {
+//     var newNums = [];
+
+//     for (var i = 0; i < nums.length; i++) {
+//         if (i === 0) {
+//             newNums.push(-1);
+//         } else {
+//             var smallerPrevNums = [];
+//             for (var j = 0; j < i; j++) {
+//                 if (nums[j] < nums[i]) {
+//                     smallerPrevNums.push(nums[j]);
+//                 } 
+//             }
+
+//             if (smallerPrevNums.length === 0) {
+//                 newNums.push(-1);
+//             } else {
+//                 newNums.push(smallerPrevNums[smallerPrevNums.length - 1]);
+//             } 
+//         }
+//     }
+
+//     return newNums;
+// }
+
+// -------UI------- // 
+var addBtn = document.getElementById("submit-input");
+var submitBtn = document.getElementById("submit");
+var inputEl = document.getElementById("input");
+var arrEl = document.getElementById("integer-arr");
+var newArrEl = document.getElementById("new-arr");
+
 function arrayPreviousLess(nums) {
     var newNums = [];
 
