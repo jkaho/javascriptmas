6da03421648ba347a71777c52256f72eb3b35993
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 18:19:38 +1100

    16: Modify js to add solution, modify html to fix typo

--- challenge-17/assets/js/script-17.js
+++ challenge-17/assets/js/script-17.js
@@ -1,3 +1,14 @@
 // -------Challenge------- // 
 // Given a string, find the number of different characters in it 
 
+// -------My Solution------- // 
+function differentSymbolsNaive(str) {
+    var chars = [];
+    for (var i = 0; i < str.length; i++) {
+        if (chars.includes(str[i]) === false) {
+            chars.push(str[i])
+        }
+    }
+    return chars.length;
+}
+
