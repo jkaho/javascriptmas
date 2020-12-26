90a4250697f1248604e32386f3cce51cffbd5a30
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 13:30:47 +1100

    15: Modify js to add variables for previous and next btns

--- challenge-15/assets/js/script-15.js
+++ challenge-15/assets/js/script-15.js
@@ -1,3 +1,7 @@
 // -------Challenge------- // 
 // Use JS to make the image carousel function
 // Left & right arrows should work 
+
+// -------My Solution------- // 
+var previousBtn = document.getElementsByClassName("previous");
+var nextBtn = document.getElementsByClassName("next");
