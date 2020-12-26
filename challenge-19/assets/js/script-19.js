afa1b670c8157e900329b2d5f3edd257b6aaa208
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 02:09:25 +1100

    19: Modify js to add UI functionality

--- challenge-19/assets/js/script-19.js
+++ challenge-19/assets/js/script-19.js
@@ -4,20 +4,49 @@
 // If there are duplicate characters, output should be false
 
 // -------My Solution------- // 
-function alphabetSubsequence(str) {
+// function alphabetSubsequence(str) {
+//     var chars = [];
+//     var splitStr = str.split("");
+
+//     for (var i = 0; i < splitStr.length; i++) {
+//         if (i !==  splitStr.length && str.charCodeAt(i) > str.charCodeAt(i + 1)) {
+//             return false;
+//         } else if (chars.includes(splitStr[i]) === true) {
+//             return false;
+//         } else {
+//             chars.push(splitStr[i]);
+//             if (chars.length === splitStr.length) {
+//                 return true;
+//             }
+//         } 
+//     }
+// }
+
+// -------UI------- // 
+var submitBtn = document.getElementById("submit");
+var inputEl = document.getElementById("input");
+var validatorEl = document.getElementById("validator");
+
+function alphabetSubsequence() {
+    var str = inputEl.value.toLowerCase();
     var chars = [];
     var splitStr = str.split("");
+    var alphabet = "abcdefghijklmnopqrstuv";
 
     for (var i = 0; i < splitStr.length; i++) {
-        if (i !==  splitStr.length && str.charCodeAt(i) > str.charCodeAt(i + 1)) {
-            return false;
+        if (alphabet.includes(splitStr[i] === false)) {
+            validatorEl.textContent = "Your string, " + "'" + inputEl.value + "'" + ", does not meet the criteria.";
+        } else if (i !==  splitStr.length && str.charCodeAt(i) > str.charCodeAt(i + 1)) {
+            validatorEl.textContent = "Your string, " + "'" + inputEl.value + "'" + ", does not meet the criteria.";
         } else if (chars.includes(splitStr[i]) === true) {
-            return false;
+            validatorEl.textContent = "Your string, " + "'" + inputEl.value + "'" + ", does not meet the criteria.";
         } else {
             chars.push(splitStr[i]);
             if (chars.length === splitStr.length) {
-                return true;
+                validatorEl.textContent = "Your string, " + "'" + inputEl.value + "'" + ", meets the criteria!";
             }
         } 
     }
 }
+
+submitBtn.addEventListener("click", alphabetSubsequence);
