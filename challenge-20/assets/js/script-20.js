791ea3b2b356e884d53a7bca8e8d22a18d6a7e8d
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 02:16:10 +1100

    20: Add 'challenge-20' w/ basic html & js files

---
+++ challenge-20/assets/js/script-20.js
@@ -0,0 +1,12 @@
+// -------Challenge------- // 
+// GoDaddy makes a lot of different top-level domains available to its customers
+// A top-level domain is one that goes directly after the last dot ('.') in the domain name
+// Example: .com in example.com
+/* To help users choose from available domains, GoDaddy is introducing a new feature 
+    that shows the type of the chosen top-level domain
+*/
+// You have to implement this feature
+/* To begin with, you want to write a function that labels the domains as "commercial",
+    "organization", "network" or "information" for .com, .org, .net or .info, respectively
+*/
+// For the given list of domains, return the list of their labels 
