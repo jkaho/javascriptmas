eb653dbc6a9d36e60afb06788abeb2cf3d819890
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 18:29:21 +1100

    18: Add 'challenge-18' folder w/ basic html & js files

---
+++ challenge-18/assets/js/script-18.js
@@ -0,0 +1,6 @@
+// -------Challenge------- // 
+/* Given an array of integers, for each position i, search among the previous
+    positions for the last (from the left) position that contains a smaller value
+*/
+// Store that value at position i in the anwer
+// If no such value can be found, store -1 instead
