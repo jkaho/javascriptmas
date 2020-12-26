b12bc1acf6edac97aab62240d1a55a3fa2802591
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 13:44:26 +1100

    15: Modify js to add event listener to next btn

--- challenge-15/assets/js/script-15.js
+++ challenge-15/assets/js/script-15.js
@@ -1,14 +1,21 @@
 // -------Challenge------- // 
 // Use JS to make the image carousel function
 // Left & right arrows should work 
+// When at first img, previous btn should be dimmed out
+// When at lsat img, next btn should be dimmed out
+
 
 // -------My Solution------- // 
 var nextBtn = document.getElementById("next");
 var previousBtn = document.getElementById("previous");
 var gallery = document.getElementById("gallery");
 
+var transform = 220;
+
 function nextImg() {
-    gallery.setAttribute("style", "transform: translateX(-220px)");
+    console.log(transform);
+    gallery.setAttribute("style", "transform: translateX(-" + transform + "px)");
+    transform += 220;
 }
 
-
+nextBtn.addEventListener("click", nextImg);
