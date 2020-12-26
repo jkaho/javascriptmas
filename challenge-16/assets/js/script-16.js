bbcb27175452af0a27df63ebe3bf66c5849f8394
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-26 18:03:09 +1100

    16: Add solution

--- challenge-16/assets/js/script-16.js
+++ challenge-16/assets/js/script-16.js
@@ -1,3 +1,19 @@
 // -------Challenge------- // 
 // Transform a given sentence into a new one with dashes between each two consecutive letters
 // Leave spaces alone 
+
+// -------My Solution------- // 
+function insertDashes(sentence) {
+    var result = [];
+    var arr = sentence.split(" ");
+
+    for (var i = 0; i < arr.length; i++) {
+        var groupOfLetters = arr[i].split("");
+        var joinedGroupOfLetters = groupOfLetters.join("-");
+        result.push(joinedGroupOfLetters);
+    }
+
+    var joinedResult = result.join(" ");
+    return joinedResult;
+}
+
