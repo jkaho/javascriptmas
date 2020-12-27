9834fef8a0772b54dcbad8a2fd4bb328e81d68ff
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 18:47:05 +1100

    22: Add 'challenge-22' folder w/ basic html & js files

---
+++ challenge-22/assets/js/script-22.js
@@ -0,0 +1,6 @@
+// -------Challenge------- // 
+/* 
+    Given a rectangular matrix and an integer column, return an array contianing 
+    the elements of the columnth column of the given matrix (the leftmost column
+    is the 0th one)
+*/
