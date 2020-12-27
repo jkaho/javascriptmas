381b16fed7580c71fc1ffacc9e1faebe4d4fe260
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 19:30:05 +1100

    22: Modify js to add event listener to 'Add to Matrix' btn

--- challenge-22/assets/js/script-22.js
+++ challenge-22/assets/js/script-22.js
@@ -27,5 +27,36 @@ var colEl = document.getElementById("col");
 var matrixEl = document.getElementById("matrix");
 var extractedArrEl = document.getElementById("extracted-arr");
 
+var matrix = [];
 
+function addArrToMatrix() {
+    var arr = integerArrEl.value.split(",");
+    var intArr = [];
+    console.log(arr);
 
+    var alphabet = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVQXYZ";
+    var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            if (alphabet.includes(arr[i][j]) || specialChars.includes(arr[i][j])) {
+                alert("Invalid input. Only integers separated by commas are accepted.");
+                return;
+            }
+        } 
+
+        var integer = parseInt(arr[i]);
+        if (integer !== NaN) {
+            intArr.push(integer);
+        } else {
+            alert("Invalid input. Only integers separated by commas are accepted.");
+            return;
+        }
+    }
+
+    matrix.push(intArr);
+    matrixEl.textContent = matrix;
+    integerArrEl.value = "";
+}
+
+addBtn.addEventListener("click", addArrToMatrix);
