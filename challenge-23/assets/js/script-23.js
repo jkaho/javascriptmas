c935327b85627f13b5973f01f6463f2d455f841f
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-28 01:20:22 +1100

    23: Add 'challenge-23' folder w/ provided html, css & empty js

---
+++ challenge-23/assets/js/script-23.js
@@ -0,0 +1,6 @@
+// -------Challenge------- // 
+// Use JS to count characters
+// Dynamically show the characters used / characters remaining
+// Disable the Tweet button if maximum character limit is exceeded
+// When character count is below 20 characters, character count turns red
+// Hints: keydown event listener & input.value.length property
