7a637744fcd88d62e4d705be377ef659584ea741
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 12:04:17 +1100

    21: Add 'challenge-21' folder w/ basic html & js files

---
+++ challenge-21/assets/js/script-21.js
@@ -0,0 +1,7 @@
+// -------Challenge------- // 
+// You have two integer arrays, a and b, and an integer target value, v
+/* 
+    Determine whether there is a pair of numbers, where one number is taken from a and
+    the other from b, that can be added together to get a sum of v
+*/
+// Return true if such a pair exists, otherwise return false 
