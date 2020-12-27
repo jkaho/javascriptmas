16fc56e0e5b89dff15803909f0c420d96f8f6c81
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 18:51:42 +1100

    22: Add solution

--- challenge-22/assets/js/script-22.js
+++ challenge-22/assets/js/script-22.js
@@ -4,3 +4,15 @@
     the elements of the columnth column of the given matrix (the leftmost column
     is the 0th one)
 */
+
+function extractMatrixColumn(matrix, column) {
+    var result = [];
+
+    for (var i = 0; i < matrix.length; i ++) {
+        var colNumber = matrix[i][column];
+        result.push(colNumber);
+    }
+    
+    return result;
+}
+
