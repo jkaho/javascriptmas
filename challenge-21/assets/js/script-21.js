849a93f5da9039447e9433e067726d22ff06ca4d
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 12:23:55 +1100

    21: Modify js to add html element variables

--- challenge-21/assets/js/script-21.js
+++ challenge-21/assets/js/script-21.js
@@ -7,6 +7,32 @@
 // Return true if such a pair exists, otherwise return false 
 
 // -------My Solution------- // 
+// function sumOfTwo(nums1, nums2, value) {
+//     var sums = [];
+
+//     for (var i = 0; i < nums1.length; i++) {
+//         for (var j = 0; j < nums2.length; j++) {
+//             var num1PlusNum2 = nums1[i] + nums2[j];
+//             sums.push(num1PlusNum2);
+//         }
+//     }
+
+//     if (sums.includes(value)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// -------UI------- // 
+var addToArray1Btn = document.getElementById("submit-num1");
+var addToArray2Btn = document.getElementById("submit-num2");
+var submitBtn = document.getElementById("submit");
+var arr1InputEl = document.getElementById("num1");
+var arr2InputEl = document.getElementById("num2");
+var valueInputEl = document.getElementById("value");
+var sumCheckerEl = document.getElementById("sum-checker");
+
 function sumOfTwo(nums1, nums2, value) {
     var sums = [];
 
