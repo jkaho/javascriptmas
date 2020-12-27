55dd66a3309e1c4d27cf71bb4969bd8d6da9489b
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 19:02:01 +1100

    22: Modify js to add html element variables

--- challenge-22/assets/js/script-22.js
+++ challenge-22/assets/js/script-22.js
@@ -5,6 +5,7 @@
     is the 0th one)
 */
 
+// -------My Solution------- // 
 function extractMatrixColumn(matrix, column) {
     var result = [];
 
@@ -12,7 +13,19 @@ function extractMatrixColumn(matrix, column) {
         var colNumber = matrix[i][column];
         result.push(colNumber);
     }
-    
+
     return result;
 }
+
+// -------UI------- // 
+var addBtn = document.getElementById("submit-arr");
+var removeLastBtn = document.getElementById("remove-last");
+var clearBtn = document.getElementById("clear-all");
+var submitBtn = document.getElementById("submit");
+var integerArrEl = document.getElementById("arr");
+var colEl = document.getElementById("col");
+var matrixEl = document.getElementById("matrix");
+var extractedArrEl = document.getElementById("extracted-arr");
+
+
 
