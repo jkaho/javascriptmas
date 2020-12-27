95b518c08006789e35018744abf61bbccadfba90
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-27 12:14:03 +1100

    21: Add solution

--- challenge-21/assets/js/script-21.js
+++ challenge-21/assets/js/script-21.js
@@ -5,3 +5,21 @@
     the other from b, that can be added together to get a sum of v
 */
 // Return true if such a pair exists, otherwise return false 
+
+// -------My Solution------- // 
+function sumOfTwo(nums1, nums2, value) {
+    var sums = [];
+
+    for (var i = 0; i < nums1.length; i++) {
+        for (var j = 0; j < nums2.length; j++) {
+            var num1PlusNum2 = nums1[i] + nums2[j];
+            sums.push(num1PlusNum2);
+        }
+    }
+
+    if (sums.includes(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
