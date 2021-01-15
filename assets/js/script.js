0a0bff9367302a1865b8125c17e2c16eed8d47be
jkaho <72959522+jkaho@users.noreply.github.com>
2021-01-16 01:17:32 +1100

    JS: Edit header btn colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 var headerBtns = document.querySelectorAll(".challenge-icon");
-console.log(headerBtns[0].firstElementChild.textContent);
+var colours = ["rgb(177, 73, 101)", "rgb(233, 147, 18)", "rgb(66, 145, 111)", "rgb(73, 97, 176)", "rgb(124, 91, 162)", "rgb(136, 69, 121)"];
+var colourIndex = 0;
+
+for (var i = 0; i < headerBtns.length; i++) {
+    headerBtns[i].setAttribute("style", "background: " + colours[colourIndex]);
+    colourIndex++;
+
+    if (colourIndex === 6) {
+        colourIndex = 0;
+    }
+}
